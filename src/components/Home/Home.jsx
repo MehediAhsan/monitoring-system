05d056155b28087f0c5858cccb3cf7ec8b696a7e
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-11-07 22:24:00 +0600

    basic setup

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <button type="button" className="btn btn-primary">Primary</button>
+        </div>
+    );
+};
+
+export default Home;
