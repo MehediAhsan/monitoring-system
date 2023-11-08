da4abf10a9759c499eab72fed365f04d43c7eef9
MehediAhsan <mdmehedicse01@gmail.com>
2023-11-08 12:04:38 +0600

    added common components

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -2,14 +2,14 @@ import bannerImg from '../../assets/banner.png'
 
 const Banner = () => {
     return (
-        <div className=" col-xxl-8 px-4 py-5 bg-info bg-gradient w-full " style={{ transform: [{ skewY: '50deg' }] }}>
+        <div className="col-xxl-8 px-4 py-5 bg-info bg-gradient w-full ">
     <div className="container mx-auto row flex-lg-row-reverse align-items-center g-5 py-5">
       <div className="col-10 col-sm-8 col-lg-6">
-        <img src={bannerImg} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
+        <img src={bannerImg} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes"  loading="lazy"/>
       </div>
-      <div className="col-lg-6">
-        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">SMART TRANSFORMER
-            MONITORING SYSTEM</h1>
+      <div className="col-lg-6 text-light">
+        <h1 className="fw-semibold lh-1 mb-3">SMART TRANSFORMER</h1>
+        <h1 className="fw-semibold lh-1 mb-3">MONITORING SYSTEM</h1>
       </div>
     </div>
   </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,43 +1,66 @@
 import Banner from "./Banner";
-import imgBody from '../../assets/img4.png';
+import imgBody from "../../assets/img4.png";
+import Common from "../common/Common";
+import imgStatus from "../../assets/img5.png";
+import imgTimeSeries from "../../assets/img1.jpeg";
+import imgDetailsStatus from "../../assets/img2.png";
+import imgSummary from "../../assets/img3.png";
 
 const Home = () => {
-    const items = [
-        'Phase 1',
-        'Phase 2',
-        'Phase 3',
-        'Line Parameter 1',
-        'Line Parameter 2',
-        'Line Parameter 3',
-      ];
-    return (
-        <div>
-            <Banner></Banner>
-            {/* FeatureImage */}
-            <img src={imgBody} alt="body" className="w-100 p-5 shadow-lg" />
-            {/* Measures Items */}
-            <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-evenly bg-info bg-gradient">
-  
-  <div className="item-list">
-      <h2 className="text-warning-emphasis fs-3">Measure Items(Phase &
-Line Parameter)</h2>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index} className="fs-5">{item}</li>
-        ))}
-      </ul>
+  const items = [
+    "Phase 1",
+    "Phase 2",
+    "Phase 3",
+    "Line Parameter 1",
+    "Line Parameter 2",
+    "Line Parameter 3",
+  ];
+  return (
+    <div>
+      <Banner></Banner>
+      {/* FeatureImage */}
+      <img src={imgBody} alt="body" className="w-100 p-5" />
+      {/* Measures Items */}
+      <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-evenly bg-primary bg-gradient my-5">
+        <div className="item-list">
+          <h2 className="text-warning fs-3">
+            Measure Items(Phase & Line Parameter)
+          </h2>
+          <ul className="text-light">
+            {items.map((item, index) => (
+              <li key={index} className="fs-5">
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="item-list">
+          <h2 className="text-warning fs-3">Safety Monitoring</h2>
+          <ul style={{ listStyleType: "square" }} className="text-light">
+            {items.map((item, index) => (
+              <li key={index} className="fs-5">
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <h1 className="text-primary text-center my-5">REMOTE MONITORING & REPORT SYSTEM</h1>
+
+      {/* Multiple Transformer Status */}
+      <Common title={`Multiple Transformer Status`} img={imgStatus} divClass={`flex-lg-row`}></Common>
+
+      {/* Time Series Transformers Parameter */}
+      <Common title={`Time Series Transformers Parameter`} img={imgTimeSeries} divClass={`flex-lg-row-reverse`}></Common>
+
+      {/* Transformer Details Status */}
+      <Common title={`Transformer Details Status`} img={imgDetailsStatus} divClass={`flex-lg-row`}></Common>
+
+      {/* Transformers Summary */}
+      <Common title={`Transformers Summary`} img={imgSummary} divClass={`flex-lg-row-reverse`}></Common>
     </div>
-    <div className="item-list">
-      <h2 className="text-warning-emphasis fs-3">Safety Monitoring</h2>
-      <ul style={{listStyleType:'square'}}>
-        {items.map((item, index) => (
-          <li key={index} className="fs-5">{item}</li>
-        ))}
-      </ul>
-    </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Home;
