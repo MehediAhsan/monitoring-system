deb547a038e12113cf62a1310dbd9e5a6afb01b1
MehediAhsan <mdmehedicse01@gmail.com>
2023-11-08 11:21:20 +0600

    updated

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,10 +1,11 @@
+import bannerImg from '../../assets/banner.png'
 
 const Banner = () => {
     return (
-        <div className=" col-xxl-8 px-4 py-5 bg-info" style={{ transform: [{ skewY: '50deg' }] }}>
+        <div className=" col-xxl-8 px-4 py-5 bg-info bg-gradient w-full " style={{ transform: [{ skewY: '50deg' }] }}>
     <div className="container mx-auto row flex-lg-row-reverse align-items-center g-5 py-5">
       <div className="col-10 col-sm-8 col-lg-6">
-        <img src="https://images.unsplash.com/photo-1588200908342-23b585c03e26?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
+        <img src={bannerImg} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
       </div>
       <div className="col-lg-6">
         <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">SMART TRANSFORMER
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,42 @@
 import Banner from "./Banner";
-
+import imgBody from '../../assets/img4.png';
 
 const Home = () => {
+    const items = [
+        'Phase 1',
+        'Phase 2',
+        'Phase 3',
+        'Line Parameter 1',
+        'Line Parameter 2',
+        'Line Parameter 3',
+      ];
     return (
         <div>
             <Banner></Banner>
-        </div>
+            {/* FeatureImage */}
+            <img src={imgBody} alt="body" className="w-100 p-5 shadow-lg" />
+            {/* Measures Items */}
+            <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-evenly bg-info bg-gradient">
+  
+  <div className="item-list">
+      <h2 className="text-warning-emphasis fs-3">Measure Items(Phase &
+Line Parameter)</h2>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index} className="fs-5">{item}</li>
+        ))}
+      </ul>
+    </div>
+    <div className="item-list">
+      <h2 className="text-warning-emphasis fs-3">Safety Monitoring</h2>
+      <ul style={{listStyleType:'square'}}>
+        {items.map((item, index) => (
+          <li key={index} className="fs-5">{item}</li>
+        ))}
+      </ul>
+    </div>
+  </div>
+</div>
     );
 };
 
